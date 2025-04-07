29d9940bc63c153edd98e06922d22ea337ed44bb
DESKTOP-0C7D4H8\it <makoid0104@gmail.com>
2025-04-07 16:49:27 +0900

    2025-04-07 16:48 학원에서 커밋

---
+++ workspace/itemEx/src/main/java/com/example/itemex/ItemExApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemExApplication.class, args);
+    }
+
+}
---
+++ workspace/itemEx/src/test/java/com/example/itemex/ItemExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
